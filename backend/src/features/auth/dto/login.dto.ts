a21d34cc10c7e3fc67edbb93e796bc0774dd55fa
KaviyaRajan29 <kaviyarajan59@gmail.com>
2026-02-13 06:40:01 +0530

    update\fix

--- backend/src/features/auth/dto/login.dto.ts
+++ backend/src/features/auth/dto/login.dto.ts
@@ -2,12 +2,12 @@ import { IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 /** DTO for user login requests. */
 export class LoginDto {
-    @IsString()
-    @IsNotEmpty()
-    username!: string;
+  @IsString()
+  @IsNotEmpty()
+  username!: string;
 
-    @IsString()
-    @IsNotEmpty()
-    @MinLength(4)
-    password!: string;
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(4)
+  password!: string;
 }
