d6bcb4e6ede0488dc315a368fee3f04d323219ab
Pemakaran <Pemakaran99@gmail.com>
2026-03-20 06:50:01 +0530

    update

--- verify_admin.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker exec ledgerpro-backend npm run typeorm -- query << 'SQL'
-SELECT id, email, password_hash, first_name, last_name, role, is_verified, is_first_login FROM users WHERE email = 'admin@ledgerpro.com';
-SQL
