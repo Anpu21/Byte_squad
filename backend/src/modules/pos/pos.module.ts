88b2b4bc793ffa0d4276e3b099bf06324f20643d
Pemakaran <Pemakaran99@gmail.com>
2026-03-20 10:42:04 +0530

    update

--- backend/src/modules/pos/pos.module.ts
+++ backend/src/modules/pos/pos.module.ts
@@ -4,9 +4,10 @@ import { PosService } from '@pos/pos.service';
 import { PosController } from '@pos/pos.controller';
 import { Transaction } from '@pos/entities/transaction.entity';
 import { TransactionItem } from '@pos/entities/transaction-item.entity';
+import { LedgerEntry } from '@accounting/entities/ledger-entry.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Transaction, TransactionItem])],
+  imports: [TypeOrmModule.forFeature([Transaction, TransactionItem, LedgerEntry])],
   controllers: [PosController],
   providers: [PosService],
   exports: [PosService],
